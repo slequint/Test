0f0303e660a36e7f15e55591feef7dcc67aa07e6
slequint <58738161+slequint@users.noreply.github.com>
2024-10-28 12:41:32 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include "header.php";
+// include "menu2.php";
+include_once "ConnBD.php";
+?>
+
+<!DOCTYPE html>
+
+<html>
+<body>
+	<div class="centre">
+	<img src="images/LoginScreen.png" alt="Image" onmouseover="javascript:this.src='images/LoginScreen2.png';" onmouseout="javascript:this.src='images/LoginScreen.png';" />
+   
+   </div>
+
+	
+	 </body>
+</html>
+<?php
+include "footer.php";
+?>
